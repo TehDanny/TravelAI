2ad68112fc6d84b74296e6e7be7396bb92a34ded
morten_clausen <mortenclausen@gmail.com>
2017-05-29 12:50:51 +0200

    Added DataPopulator. Person class and methods for generating people

---
+++ DataPopulator/Person.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPopulator
+{
+    class Person
+    {
+        static Random rand = new Random();
+        public int Age { get; set; }
+        public int AnnualIncome { get; set; } // in thousands
+        public int WorkStatus { get; set; } // 1= Student, 2 = Employed, 3=unemployed, 4= Retired
+        //public int Sex { get; set; } // 1= male, 2= female
+        public int Destination { get; set; } // 1 = cheapest and youngest, 10 oldest and most expensive
+
+        public Person()
+        {
+            
+            
+            this.Age = rand.Next(16, 81);
+            //this.Sex = rand.Next(1, 3);
+            if (this.Age >= 67)
+            {
+
+                this.WorkStatus = 4;
+                this.AnnualIncome = rand.Next(150, 250) * 1000;
+            }
+            else if (this.Age >= 40)
+            {
+                this.WorkStatus = rand.Next(2, 4);
+                if (this.WorkStatus == 2)
+                    this.AnnualIncome = rand.Next(300, 500) * 1000;
+                else
+                    this.AnnualIncome = rand.Next(150, 250) * 1000;
+            }
+            else if (this.Age >= 25)
+            {
+                this.WorkStatus = rand.Next(1, 4);
+                if (this.WorkStatus == 1)
+                    this.AnnualIncome = rand.Next(120, 150) * 1000;
+                else if (this.WorkStatus == 2)
+                    this.AnnualIncome = rand.Next(250, 400) * 1000;
+                else
+                    this.AnnualIncome = rand.Next(150, 250) * 1000;
+            }
+            else if (this.Age >= 18) // 18-25
+            {
+                this.WorkStatus = rand.Next(1, 4);
+                if (this.WorkStatus == 1)
+                    this.AnnualIncome = rand.Next(60, 150) * 1000;
+                else if (this.WorkStatus == 2)
+                    this.AnnualIncome = rand.Next(200, 300) * 1000;
+                else
+                    this.AnnualIncome = rand.Next(150, 200) * 1000;
+            }
+            else // 16-18
+            {
+                this.WorkStatus = rand.Next(1, 4);
+                if (this.WorkStatus == 1)
+                    this.AnnualIncome = rand.Next(0, 20) * 1000;
+                else if (this.WorkStatus == 2)
+                    this.AnnualIncome = rand.Next(20, 75) * 1000;
+                else
+                    this.AnnualIncome = rand.Next(0, 20) * 1000;
+            }
+            this.Destination = CalculateDestination();
+        }
+        int CalculateDestination()
+        {
+            int destination = 1;
+            Random rand = new Random();
+            if(this.AnnualIncome >= 400000)
+                destination += 4;
+            else if(this.AnnualIncome >= 300000)
+                destination += 3;
+            else if(this.AnnualIncome > 200000)
+                destination += 2;
+            else if(this.AnnualIncome > 100000)
+                destination += 1;
+            
+            if(this.Age >= 60)
+                destination += 2;
+            else if(this.Age >= 40)
+                destination += 3;
+            else if(this.Age >= 30)
+                destination += 2;
+            else if(this.Age >= 25)
+                destination += 1;
+
+            if(this.WorkStatus == 2)  //Employed
+                destination += 2;
+            else if(this.WorkStatus == 4) // retired
+                destination += 1;
+            return destination;
+        }
+       
+    }
+}
--- DataPopulator/Program.cs
+++ DataPopulator/Program.cs
@@ -10,6 +10,35 @@ namespace DataPopulator
     {
         static void Main(string[] args)
         {
+            Program p = new Program();
+            p.Run();
+        }
+        void Run()
+        {
+            List<Person> customerList = GeneratePeopleData(10000);
+            printList(customerList);
+        }
+        List<Person> GeneratePeopleData(int numberOfRows)
+        {
+            List<Person> peopleList = new List<Person>();
+            
+            for(int i = 0; i< numberOfRows; i++)
+            {
+                Person person = new Person();
+                peopleList.Add(person);
+                
+            }
+            
+            return peopleList;
+        }
+        void printList(List<Person> peopleList)
+        {
+            foreach(Person person in peopleList)
+            {
+                Console.WriteLine("Age: " + person.Age + /*" Sex: " + person.Sex + */" WorkStatus: " + person.WorkStatus +
+                " Annual Income: " + person.AnnualIncome + " Destination: " + person.Destination);
+            }
+            Console.ReadKey(true);
         }
     }
 }
