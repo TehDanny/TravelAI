9783e3d0bf6e8b05fa855322c1507fd9fb3efeab
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2017-05-31 14:40:02 +0200

    Resolved merge conflict.

--- TravelAI/NormalizedCustomer.cs
+++ TravelAI/NormalizedCustomer.cs
@@ -35,25 +35,5 @@ namespace TravelAI
             WorkStatusEmployed = workStatusEmployed;
             WorkStatusRetired = workStatusRetired;
         }
-        public double DestinationPrag { get; set; }
-        public double DestinationBudapest { get; set; }
-        public double DestinationBerlin { get; set; }
-        public double DestinationStockholm { get; set; }
-        public double DestinationOslo { get; set; }
-        public double DestinationLondon { get; set; }
-        public double DestinationNewYork { get; set; }
-        public double DestinationGreenland { get; set; }
-        public double DestinationBoraBora { get; set; }
-        public double DestinationDubai { get; set; }
-
-        public NormalizedCustomer(double age, double annualIncome, double workStatusStudent, double workStatusUnemployed, double workStatusEmployed, double workStatusRetired)
-        {
-            Age = age;
-            AnnualIncome = annualIncome;
-            WorkStatusStudent = workStatusStudent;
-            WorkStatusUnemployed = workStatusUnemployed;
-            WorkStatusEmployed = workStatusEmployed;
-            WorkStatusRetired = workStatusRetired;
-        }
     }
 }
